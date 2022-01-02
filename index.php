a09fbe92771a84025a9ba6f886b56b99c6bac344
Mr-Ajith <ajithmofficial@gmail.com>
2022-01-02 17:37:57 +0530

    new

--- index.php
+++ index.php
@@ -1,8 +1,7 @@
 <html>
 <head>
 <body>
-    <h2>test</h2>
- Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit quo alias error at deleniti molestiae dolor. Repudiandae impedit nobis culpa, sit fugit dolorum laudantium deserunt illum esse unde porro repellendus.
+  work in progres
 </body>
 </head>
 </html>
